67efccce17643a0cd813da7e44dca1be8f68bf6b
Abdellahsyani <syaniabdos@gmail.com>
2024-11-19 14:40:17 +0100

    test cases

--- main.c
+++ main.c
@@ -260,6 +260,80 @@ int main()
 		fflush(stdout);
 		return 1;
 	}
+	//test char
+	//
+	/* ===========> %c <=========== */
+	printf("----------------------\n");
+	printf("----> CHAR CASES <----\n");
+	printf("----------------------\n");
+	ft_putchar(10);
+	len_1 = printf("Expected output:    %c\n", 53);
+	len_2 = ft_printf("Current output:     %c\n", 53);
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	len_1 = printf("Expected output:    %cc\n", 'a');
+	len_2 = ft_printf("Current output:     %cc\n", 'a');
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	len_1 = printf("Expected output:    %cAAA\n", 'a');
+	len_2 = ft_printf("Current output:     %cAAA\n", 'a');
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	len_1 = printf("Expected output:    %c\n", '\0');
+	len_2 = ft_printf("Current output:     %c\n", '\0');
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	len_1 = printf("Expected output:    %%%c\n", 'y');
+	len_2 = ft_printf("Current output:     %%%c\n", 'y');
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	
+	// test cases %
+	printf("----------------------\n");
+	printf("-> PERCENTAGE CASES <-\n");
+	printf("----------------------\n");
+	len_1 = printf("Expected output:    %%\n");
+	len_2 = ft_printf("Current output:     %%\n");
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	len_1 = printf("Expected output:    %%%%%%\n");
+	len_2 = ft_printf("Current output:     %%%%%%\n");
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	printf("Expected output:    ");
+	len_1 = printf("%");
+	printf("\n");
+	printf("Current output:     ");
+	len_2 = ft_printf("%");
+	printf("\n");
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	printf("Expected output:    ");
+	len_1 = printf("%   ");
+	printf("\n");
+	printf("Current output:     ");
+	len_2 = ft_printf("%   ");
+	printf("\n");
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	printf("Expected output:    ");
+	len_1 = printf("test%");
+	printf("\n");
+	printf("Current output:     ");
+	len_2 = ft_printf("test%");
+	printf("\n");
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
+	printf("Expected output:    ");
+	len_1 = printf("%  s", "valid format");
+	printf("\n");
+	printf("Current output:     ");
+	len_2 = ft_printf("%  s", "valid format");
+	printf("\n");
+	printf("Expected length:    [%d]\n", len_1);
+	printf("Current length:     [%d]\n", len_2);
 
 	return (0);
 }
